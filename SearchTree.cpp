f687e485acfc510716d51554eadaeba29127827e
NajlaHanifahMuhtadi <najlahanifahm@gmail.com>
2026-06-11 11:33:44 +0700

    menambahkan tampilan menu binnaryTree

--- SearchTree.cpp
+++ SearchTree.cpp
@@ -126,3 +126,19 @@ public:
         }
     }
 };
+
+int main()
+{
+    BinaryTree obj;
+
+    while (true)
+    {
+        cout << "\nMenu" << endl;
+        cout << "1. Implement insert operation" << endl;
+        cout << "2. Perform Inorder traversal" << endl;
+        cout << "3. Perform Preorder traversal" << endl;
+        cout << "4. Perform Postorder traversal" << endl;
+        cout << "5. Exit" << endl;
+        cout << "Enter your choice: (1-5): ";
+    }
+}
